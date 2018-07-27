46611b6bfa844fbb89e1c92ac12802bb26bb2f0b
Young <hogusong1@gmail.com>
2018-07-26 23:08:19 -0700

    Test Creating records

---
+++ test/create_test.js
@@ -0,0 +1,36 @@
+const assert = require('assert');
+const User = require('../models/user');
+
+describe('Creating records', () => {
+
+  it('Save users', done => {
+    const joe = new User({  name: 'Joseph',  gender: 'Male' });
+    const narae = new User({  name: 'Narae',  gender: 'Female' });
+    joe.save();
+    narae.save()
+      .then(() => {
+        User.find({})
+          .then(users => {
+            assert(users.length === 2);
+            assert(users[0].name === 'Joseph')
+            done();    
+          })
+      });
+  });
+
+  it('Save users by using Promise', done => {
+    const joe = new User({ name: 'Joseph',  gender: 'Male' });
+    const randy = new User({ name: 'Randy',  gender: 'Unknown' });
+    const narae = new User({ name: 'Narae',  gender: 'Female' });
+    Promise.all([ joe.save(), randy.save(), narae.save() ])
+      .then(() => {
+        User.find({})
+        .then(users => {
+          console.log(users)
+          assert(users.length === 3);
+          assert(users[0].name === 'Joseph')
+          done();    
+        })
+      })
+  });
+});
---
+++ test/test_helper.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+before(done => {
+  mongoose.connect('mongodb://localhost/user_test');
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', err => console.warn('warning', err));
+});
+
+beforeEach(done => {
+  const { users, comments, blogposts } = mongoose.connection.collections ;
+  if(users)   users.drop(() => done() );
+  if(comments) comments.drop(() => done() );
+  if(blogposts) blogposts.drop(() => done() );
+});
